e53250c13f1603c8c4227eff00fe60354553c610
Daniel Joseph Bertubin <djeoly@gmail.com>
2024-12-30 00:46:14 +0800

    Update React project with latest changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -42,24 +42,28 @@ const Login = ({ setLoggedIn }) => {
     };
 
     return (
-        <div className="login-page">
+        <div className="login-container">
             <div className="login-box">
                 <h1 className="login-title">Login</h1>
                 {error && <p className="error-message">{error}</p>}
-                <input
-                    type="text"
-                    className="login-input"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    className="login-input"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+                <div className="input-group">
+                    <input
+                        type="text"
+                        className="login-input"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="input-group">
+                    <input
+                        type="password"
+                        className="login-input"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
                 <button className="login-button" onClick={handleLogin}>
                     Login
                 </button>
