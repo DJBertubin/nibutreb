420d0a71c04f08faac0d96230275ad4be49b03af
Daniel Joseph Bertubin <djeoly@gmail.com>
2024-12-30 02:07:00 +0800

    Update React project with latest changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
 import ProductDashboard from './pages/ProductDashboard';
+import ClientDashboard from './pages/ClientDashboard'; // Import Client Dashboard
 import Login from './components/Login';
 import Signup from './components/Signup'; // Import the Signup component
 
@@ -11,8 +12,9 @@ const App = () => {
             <Routes>
                 <Route path="/admin-dashboard" element={<AdminDashboard />} />
                 <Route path="/products" element={<ProductDashboard />} />
+                <Route path="/client-dashboard" element={<ClientDashboard />} /> {/* New Client Dashboard route */}
                 <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} /> {/* New Sign Up route */}
+                <Route path="/signup" element={<Signup />} />
                 <Route path="*" element={<Login />} /> {/* Default route */}
             </Routes>
         </BrowserRouter>
