3eea79d6da39595f54935ef5742798c5f508421a
Daniel Joseph Bertubin <djeoly@gmail.com>
2024-12-30 16:26:17 +0800

    Update React project with latest changes

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,9 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: 'client' },
+    shopifyUrl: { type: String }, // Store the Shopify store URL
+    shopifyToken: { type: String }, // Store the Shopify Admin Access Token
+    shopifyData: { type: Object, default: {} }, // Store fetched Shopify data
 });
 
 module.exports = mongoose.model('User', UserSchema);
