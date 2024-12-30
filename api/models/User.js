5dacf04a1b779855a00d3164ff3feb834146055a
Daniel Joseph Bertubin <djeoly@gmail.com>
2024-12-31 03:37:20 +0800

    Update React project with latest changes

--- api/models/User.js
+++ api/models/User.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid'); // For generating unique client IDs
+const { nanoid } = require('nanoid'); // For generating unique client IDs
 
 const UserSchema = new mongoose.Schema({
-    clientId: { type: String, required: true, unique: true, default: uuidv4 }, // Unique Client ID
+    clientId: {
+        type: String,
+        required: true,
+        unique: true,
+        default: () => nanoid(), // Use nanoid to generate unique Client IDs
+    },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: 'client' },
-    shopifyUrl: { type: String }, // Optional Shopify store URL
-    shopifyToken: { type: String }, // Optional Shopify access token
-    shopifyData: { type: Object, default: {} }, // Optional Shopify data
+    shopifyData: { type: Object, default: {} }, // Store Shopify data directly in User schema
+    shopifyUrl: { type: String, unique: false }, // Optional, for store linking
+    shopifyToken: { type: String }, // Store access token
 });
 
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
